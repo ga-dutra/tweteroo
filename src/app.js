9bcb1e294631b88a71b309b40b026d74aa6fa23e
gacdutra <ga.acdutra@gmail.com>
2022-08-27 18:32:34 -0300

    adds login and displayed tweets

--- src/app.js
+++ src/app.js
@@ -5,9 +5,41 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+const serverUsers = [];
+const tweetsList = [];
+
+server.post("/sign-up", (req, res) => {
+  console.log("tentei fazer login");
+  console.log(req.body);
+  const { username, avatar } = req.body;
+  if (!username || !avatar) {
+    return res
+      .status(404)
+      .send("Para realizar o login, digite um usuário e avatar válidos");
+  }
+  serverUsers.push(req.body);
+  console.log(serverUsers);
+  res.status(201).send("OK");
+});
+
 server.get("/tweets", (req, res) => {
-  res.send("olá");
   console.log("olá console");
+  console.log(req.body);
+  const displayedTweets = tweetsList.map((tweet) => {
+    const user = serverUsers.find((user) => user.username === tweet.username);
+    return { avatar: user.avatar, ...tweet };
+  });
+  res.send(displayedTweets);
+});
+
+server.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+  if (!tweet) {
+    return res.status(404).send("Por favor, digite um tweet válido!");
+  }
+  const newTweet = { username: username, tweet: tweet };
+  tweetsList.unshift(newTweet);
+  res.status(201).send("OK");
 });
 
 server.listen(5000, () => console.log("listening on port 5000"));
